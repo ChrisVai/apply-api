987b71a87de8c2dfc83fd85e79652bbee6373455
ChrisVai <chrisbellet@gmail.com>
2024-10-31 10:38:14 +0100

    implem hashing password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,17 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { HashService } from '../auth/hash/hash.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    @Inject()
+    private hashService: HashService,
   ) {}
-  create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto) {
+    createUserDto.password = await this.hashService.hashPassword(
+      createUserDto.password,
+    );
     return this.usersRepository.save(createUserDto);
   }
 
