3f96bebf167333d7c3da6b9700e784186e0fb429
ChrisVai <chrisbellet@gmail.com>
2024-11-26 11:52:32 +0100

    adding jwtguards EVERYWHERE

--- src/applications/applications.module.ts
+++ src/applications/applications.module.ts
@@ -3,11 +3,19 @@ import { ApplicationsService } from './applications.service';
 import { ApplicationsController } from './applications.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Application } from './entities/application.entity';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Application])],
   controllers: [ApplicationsController],
-  providers: [ApplicationsService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    ApplicationsService,
+  ],
   exports: [TypeOrmModule],
 })
 export class ApplicationsModule {}
--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -3,11 +3,19 @@ import { CompaniesService } from './companies.service';
 import { CompaniesController } from './companies.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Company } from './entities/company.entity';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Company])],
   controllers: [CompaniesController],
-  providers: [CompaniesService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    CompaniesService,
+  ],
   exports: [TypeOrmModule],
 })
 export class CompaniesModule {}
